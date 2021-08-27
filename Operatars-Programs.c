5021513389e72ece4214d6c0d695ed3767cffa8b
AKSHAY MULE <akshaymule080@gmail.com>
2021-08-27 18:10:39 +0530

    Create Operatars-Programs.c

---
+++ Operatars-Programs.c
@@ -0,0 +1,66 @@
+#include<stdio.h>
+main()
+{
+   // Arithmetic Operator (+,-,*,/,%)
+
+   int a=10,b=3,c,d,e,f,g;
+
+   c=a+b;          //Addition +
+   d=a-b;          //Subcraction -
+   e=a*b;          //Multiplication *
+   f=a/b;          //Division /
+   g=a%b;          //Modulus %
+   printf("Addition is=%d\n",c);
+   printf("Subcrstion is=%d\n",d);
+   printf("Multiplication is=%d\n",e);
+   printf("Division is=%d\n",f);
+   printf("Modulus is=%d\n",g);
+}
+
+
+//Relational Operators (<,>,<=,>=,!=,==)
+#include<stdio.h>
+main()
+{
+     int a=10,b=3,c,d,e,f,g,h;
+
+   c=a<b;          //Less then <
+   d=a>b;          //Greater then >
+   e=a<=b;          //Greater then or Equal to >=
+   f=a>=b;          //Less then or Equal to <=
+   g=a!=b;          //Not Equal to !=
+   h=a==b;          //Equal to ==
+   printf("Less then is=%d\n",c);
+   printf("Greater then is=%d\n",d);
+   printf("Greater then or Equal to is=%d\n",e);
+   printf("Less then or Equal to=%d\n",f);
+   printf("Not Equal to is=%d\n",g);
+   printf("Equal to is=%d\n",h);
+
+}
+
+
+ //Logical Operators (AND &&,OR ||,NOT !)
+ #include<stdio.h>
+ main()
+ {
+     // this Operators is used in condisnal statment
+ }
+
+
+ //Assignment Operators (=,+=,-=,*=,/=)
+ #include<stdio.h>
+ main()
+ {
+     int a=7;
+     a+=1;
+     a-=1;
+     a*=2;
+     a/=7;
+     printf("%d",a);
+ }
+
+
+
+
+
