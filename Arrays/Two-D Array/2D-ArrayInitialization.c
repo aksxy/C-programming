d1212077ffc937839f9f0a58902b2896da02c363
AKSHAY MULE <akshaymule080@gmail.com>
2021-08-29 17:06:20 +0530

    changes

---
+++ Arrays/Two-D Array/2D-ArrayInitialization.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+    int arr[3][3]= {10,20,30,40,50,60,70,80,90};
+                   int i,j;
+    for(i=0; i<3; i++)
+    {
+        for(j=0; j<3; j++)
+        {
+            printf("%d ",arr[i][j]);
+        }
+        printf("\n");
+    }
+}
