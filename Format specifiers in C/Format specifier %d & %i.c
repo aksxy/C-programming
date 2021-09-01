b310b43625a2202ba5a7b3b83872e5cd791a7205
AKSHAY MULE <akshaymule080@gmail.com>
2021-09-01 20:18:00 +0530

    Format specifiers

---
+++ Format specifiers in C/Format specifier %d & %i.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    int x = 45, y = 90;
+    printf("%d\n", x);
+    printf("%i\n", x);
+    return 0;
+}
---
+++ Format specifiers in C/Format specifier %u.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    // The -10 value is converted into it's positive
+    // equivalent by %u
+    printf("%u\n", -10);
+    printf("%u\n", 10);
+    return 0;
+}
