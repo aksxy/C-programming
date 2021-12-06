45e96ff093ceb53a140d82d896cafaa81cf00899
AKSHAY MULE <akshaymule080@gmail.com>
2021-12-06 22:06:01 +0530

    Constant

---
+++ Constant in c/PredefinedConstants.c
@@ -0,0 +1,10 @@
+//CONSTANT Ex: Some Predefined macros(define name) linke __DATE__,__TIME__ .
+#include<stdio.h>
+
+main()
+{
+    printf("Date:%s\n",__DATE__);
+    printf("Time:%s\n",__TIME__);
+    return 0;
+}
+
