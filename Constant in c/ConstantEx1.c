8c875569f146169be437e89ef40b50a704e8529a
AKSHAY MULE <akshaymule080@gmail.com>
2021-12-06 22:05:43 +0530

    Constant

---
+++ Constant in c/ConstantEx1.c
@@ -0,0 +1,9 @@
+//CONSTANT Ex: Addition.
+#include<stdio.h>
+#define add(x,y) x+y
+main()
+{
+    pintf("Addition of Two No: %d",add(4,5)); //Constant not run in this system check program on online compiler.
+    return 0;
+}
+
