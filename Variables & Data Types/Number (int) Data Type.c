e2f2b7e858d40aee5420d05d9fe1e9521364e343
AKSHAY MULE <akshaymule080@gmail.com>
2021-12-10 21:46:48 +0530

    Static Variable

---
+++ Variables & Data Types/Number (int) Data Type.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+main()
+{
+    int a=1234567891;
+    short int d=12345;
+    long int b=1234567891;
+    long long int c=1234567891234567891;
+    printf("int save 10 No.%d\n",a);
+     printf("long int save 10 No.%ld\n",b);
+      printf("long long int save 19 No.%lld\n",c);
+       printf("short int save 5 No.%d\n",d);
+
+}
+
